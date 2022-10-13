5d3bc2ad82a0c1368ea6124cdb21149d6d2af46b
Simlex <gettosimlex@gmail.com>
2022-10-13 11:15:48 +0100

    Added custom cursor

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -3,11 +3,13 @@ import style from '../styles/home.module.scss';
 
 type Props = {
     children: ReactElement;
+    cursorRef: any;
 }
 
 const Container = (props: Props) => {
   return (
     <div className={style.container}>
+      <div className={style.cursor} ref={props.cursorRef}></div>
         {props.children}
     </div>
   )
