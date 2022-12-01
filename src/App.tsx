bbdff99576ded7d25dc4be0665e01d98a0cd60fa
Simlex <gettosimlex@gmail.com>
2022-12-01 12:15:29 +0100

    Refactored

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useRef, useState } from 'react';
+import React, { ReactElement, useEffect, useRef, useState } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import Home from './components/Home';
 import Cart from './components/Cart';
@@ -8,6 +8,12 @@ import Container from './components/Container';
 import { AnimatePresence } from 'framer-motion';
 import Loader from './components/Loader';
 import style from './styles/app.module.scss';
+// Images 
+import sneakers1 from '../src/sneakers/sneakers_1.png';
+import sneakers2 from '../src/sneakers/sneakers_2.png';
+import sneakers3 from '../src/sneakers/sneakers_3.png';
+import sneakers4 from '../src/sneakers/sneakers_4.png';
+import { cleanup } from '@testing-library/react';
 
 function App(): ReactElement {
   const location = useLocation();
@@ -17,28 +23,33 @@ function App(): ReactElement {
     setLoaderVisible(false);
   }, 5000);
 
+
+  // Ref hook for custom cursor 
   const cursorRef = useRef<HTMLDivElement>();
   document.addEventListener("mousemove", moveCursor);
+  // Function to move cursor 
   function moveCursor(e: any) {
     let x = e.clientX - 13;
     let y = e.clientY - 12;
 
-    if(cursorRef.current !== undefined) {
-    cursorRef.current.style.left = `${x}px`;
-    cursorRef.current.style.top = `${y}px`;
+    if (cursorRef.current !== undefined) {
+      cursorRef.current.style.left = `${x}px`;
+      cursorRef.current.style.top = `${y}px`;
     }
 
   }
 
   console.log('Location path: ', location.pathname);
-  
+
 
   return (
     <>
-    <p className={style.text}>
-      {location.pathname === '/' && 'Home'}
-      {location.pathname === '/Product' && 'Product'}
-    </p>
+      <p className={style.text}>
+        {location.pathname === '/' && 'Home'}
+        {location.pathname === '/Product' && 'Product'}
+        {location.pathname === '/Checkout' && 'Checkout'}
+        {location.pathname === '/Cart' && 'Cart'}
+      </p>
       <Container cursorRef={cursorRef}>
         <>
           {loaderVisible && (<Loader />)}
